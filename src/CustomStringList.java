cc1f2bf7a9cb57c2dd3fec3e262f3ceab11afe9d
Algalon <kirill.bernatskyi@gmail.com>
2023-12-17 12:45:38 +0100

    add check method

--- src/CustomStringList.java
+++ src/CustomStringList.java
@@ -4,4 +4,13 @@ public class CustomStringList {
     public CustomStringList(String[] list) {
         this.list = list;
     }
+
+    public boolean check(String string){
+        for (String elements: this.list ) {
+            if (string.equals(elements)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
